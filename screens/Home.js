5a430ce0ad5697252717032d0e123f4599851b04
CSPhilip <philipphoo123@gmail.com>
2023-08-24 14:00:11 +0630

    Setting up bottom navigation

---
+++ screens/Home.js
@@ -0,0 +1,11 @@
+import { View, Text, TouchableOpacity } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hello Home!</Text>
+    </View>
+  );
+
+}
